115cd8ab469d738af81a3c9271d2f3d8787273f1
Danil Komov <komovdi88@gmail.com>
2024-05-06 17:32:27 +0300

    ButcherShop

--- main.go
+++ main.go
@@ -1,48 +1,112 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
 )
 
-type User struct {
-	Name  string
-	Phone string
-	Email string
+type CustomerReviews struct {
+	Id                               uint16
+	Name, Email, SiteRating, Content string
+}
+
+var reviews = []CustomerReviews{}
+
+// подключение к бд, для получения данных о фитбеке пользователей
+func dataFromDB() []CustomerReviews {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/feetback")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `usersdata2`")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Close()
+	// проходит по таблице и забираем все данные из указанных полей
+	reviews = []CustomerReviews{}
+	for res.Next() {
+		var review CustomerReviews
+		err := res.Scan(&review.Id, &review.Name, &review.Email, &review.SiteRating, &review.Content)
+		if err != nil {
+			panic(err)
+		}
+		reviews = append(reviews, review)
+	}
+	return reviews
+
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/index.html")
-	tmpl.Execute(w, nil)
+	tmpl, err := template.ParseFiles("templates/index.html", "templates/footer.html", "templates/customerReviews.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "index", dataFromDB())
 
 }
 func menuPage(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/menu.html")
-	tmpl.Execute(w, nil)
+	tmpl, err := template.ParseFiles("templates/menu.html", "templates/footer.html", "templates/customerReviews.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "menu", dataFromDB())
 }
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/about.html")
+	tmpl, err := template.ParseFiles("templates/about.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 	tmpl.Execute(w, nil)
 }
 func contactsPage(w http.ResponseWriter, r *http.Request) {
-<<<<<<< HEAD
-	//userBob := User{"Bob", "+7-123-456-7890", "bob@gmail.com"}
-	fmt.Fprintf(w, "contact page")
-	//fmt.Fprintf(w, "User data: %s %s %s", userBob.name, userBob.phone, userBob.email)
-=======
-	userBob := User{"Bob", "+7-123-456-7890", "bob@gmail.com"}
-	tmpl, _ := template.ParseFiles("templates/contact.html")
-	tmpl.Execute(w, userBob)
+	tmpl, err := template.ParseFiles("templates/contact.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "contact", nil)
 }
 func chefsPage(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/stuff.html")
+	tmpl, err := template.ParseFiles("templates/stuff.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 	tmpl.Execute(w, nil)
 }
 func galleryPage(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/gallery.html")
-	tmpl.Execute(w, nil)
->>>>>>> template
+	tmpl, err := template.ParseFiles("templates/gallery.html", "templates/footer.html", "templates/customerReviews.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "gallery", dataFromDB())
+}
+
+func saveFeetback(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	siteRating := r.FormValue("siterating")
+	content := r.FormValue("content")
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/feetback")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	// Create a new user data
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `usersdata2` (`name`, `email`, `siteRating`, `content`) VALUES ('%s', '%s', '%s', '%s')", name, email, siteRating, content))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func handleRequest() {
@@ -53,6 +117,7 @@ func handleRequest() {
 	mux.HandleFunc("/contacts/", contactsPage)
 	mux.HandleFunc("/chefs/", chefsPage)
 	mux.HandleFunc("/gallery/", galleryPage)
+	mux.HandleFunc("/saveFeetback/", saveFeetback)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 
